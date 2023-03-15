a51f8453a087beb5e4228e4c8888f3e301c32e68
Ziheng Gao <gaoziheng@bytedance.com>
2023-03-15 11:00:31 +0800

    feat(typings): add http method typings

--- typings/http.ts
+++ typings/http.ts
@@ -1,5 +1,12 @@
 export interface HttpInstance {
     request<T = any, R = T, D = any>(opts: HttpRequestOptions<D>): Promise<R>;
+	get<T = any, R = T, D = any>(url: string, opts?: HttpRequestOptions<D>): Promise<R>;
+	delete<T = any, R = T, D = any>(url: string, opts?: HttpRequestOptions<D>): Promise<R>;
+	head<T = any, R = T, D = any>(url: string, opts?: HttpRequestOptions<D>): Promise<R>;
+	options<T = any, R = T, D = any>(url: string, opts?: HttpRequestOptions<D>): Promise<R>;
+	post<T = any, R = T, D = any>(url: string, data?: D, opts?: HttpRequestOptions<D>): Promise<R>;
+	put<T = any, R = T, D = any>(url: string, data?: D, opts?: HttpRequestOptions<D>): Promise<R>;
+	patch<T = any, R = T, D = any>(url: string, data?: D, opts?: HttpRequestOptions<D>): Promise<R>;
 }
 
 export type ResponseType =
