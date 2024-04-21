a1ee2b17d616ac3cf01fa3234f92e69b6b71ee39
yunnysunny <yunnysunny@gmail.com>
2024-04-21 23:12:55 +0800

    ci: change package name in js

---
+++ .github/workflows/pkg.js
@@ -0,0 +1,14 @@
+var data = require('../../package.json');
+var fs = requrie('fs');
+var path = require('path');
+//Manipulate data
+data.name = '@whyun/feishu-node-sdk';
+data.version = process.env.PKG_VERSION || data.version;
+if (data.version.startsWith('v')) {
+    data.version = data.version.slice(1);
+}
+fs.writeFileSync(
+    path.join(__dirname, '../../package.json'),
+    JSON.stringify(data, null, 2),
+    'utf-8'
+)
